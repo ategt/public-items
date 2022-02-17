b09b384aab6e88b50d43584ec13ad705ff1fc417
Adam Tegtmeier <ategtmei@gmail.com>
2022-02-17 13:15:35 -0500

    Fixed the screen spew problem with webp_to_gif.py, but it is still a bit ugly.  Both the code and the output.

--- server/webp_to_gif.py
+++ server/webp_to_gif.py
@@ -32,24 +32,41 @@ os.mkdir(temp_directory)
 
 pngs = list()
 
-for frame in frames:
-    frameNumber = frame['No']
-    animationFrame_to_image__command = [webpmuxPath, "-get", "frame", str(frameNumber), sourceFile, "-o", "-"]    
-    webpImageData = subprocess.check_output(animationFrame_to_image__command)
-    
+def decompress(webpImageData):
     webp_to_png_command = [dwebpPath, "-o", "-", "--", "-"]
     proc = subprocess.Popen(webp_to_png_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate(webpImageData)
 
+    return stdout, stderr
+
+def demux(frameNumber):
+    animationFrame_to_image__command = [webpmuxPath, "-get", "frame", str(frameNumber), sourceFile, "-o", "-"]
+    #webpImageData = subprocess.check_output(animationFrame_to_image__command)
+    proc = subprocess.Popen(animationFrame_to_image__command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+
+    return stdout, stderr
+
+def saveImage(path, data):
+    with open(path, 'wb') as handle:
+        handle.write(data)
+
+for frame in frames:
+    frameNumber = frame['No']
+
+    webpImageData, stderr1 = demux(frameNumber)
+    pngImageData, stderr2 = decompress(webpImageData)
+
+    print("\r", stderr1.decode("UTF-8").replace("\n","-").replace("\r",""),"\t=2=", stderr2.decode("UTF-8").replace("\n", "-").replace("\r",""), end="")
+    
     output_png_path = os.path.join(temp_directory,f"frame-{frameNumber}.png")
-
-    with open(output_png_path, 'wb') as handle:
-        handle.write(stdout)
-
+    saveImage(output_png_path, pngImageData)
     pngs.append(output_png_path)
 
 input_png_files = os.path.join(temp_directory, "frame-*.png")
 gifski_command = [gifski_path, input_png_files, "--fps", "20", "--quality", "90", "--output", "-"]
+
+print("\nGenerating GIF...")
 
 gifski_output = subprocess.check_output(gifski_command)
 
